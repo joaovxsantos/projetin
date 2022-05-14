f8732d9cfd0735a4de779e11bdf3393bb793de78
João Vitor <joaovitor55670@gmail.com>
2022-05-14 17:18:44 -0300

    commit final

---
+++ script.js
@@ -0,0 +1,15 @@
+function um(){
+    let a = document.getElementById("one")
+    let b = document.getElementById("titulo")
+    a.innerText = b.value
+    
+    
+    
+}
+
+
+function dois(){
+    let a = document.getElementById("two")
+    let b = document.getElementById("texto")
+    a.innerHTML = b.value
+}
